fd9c3ecfb098771d513aa1457cb9a85c14051364
Luo J.Q <37497646+peterljq@users.noreply.github.com>
2018-08-23 23:36:28 +0800

    Update 102. Linked List Cycle.cpp

--- LintCode/102. Linked List Cycle.cpp
+++ LintCode/102. Linked List Cycle.cpp
@@ -1,30 +1,32 @@
 /**
- * Definition for ListNode.
- * public class ListNode {
+ * Definition of ListNode
+ * class ListNode {
+ * public:
  *     int val;
- *     ListNode next;
+ *     ListNode *next;
  *     ListNode(int val) {
- *         this.val = val;
- *         this.next = null;
+ *         this->val = val;
+ *         this->next = NULL;
  *     }
  * }
  */
 
 
-public class Solution {
+class Solution {
+public:
     /*
      * @param head: The first node of linked list.
      * @return: True if it has a cycle, or false
      */
-    public boolean hasCycle(ListNode head) {
+    bool hasCycle(ListNode * head) {
         // write your code here
-        ListNode lets_run = head;
-        for(int i = 0; i < 20000; i++){
-            if(lets_run == null){
+        ListNode *runrunrun = head;
+        for(int i = 0; i < 65536; i++){
+            if(runrunrun == NULL){
                 return false;
             }
-            lets_run = lets_run.next;
+            runrunrun = runrunrun -> next;
         }
         return true;
     }
-}
+};
